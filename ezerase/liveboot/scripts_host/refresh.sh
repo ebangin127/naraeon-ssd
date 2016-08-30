f7fcb32c8c5c2ff7bf703cddd01f6b510bdd7b42
Minkyu Kim <external@naraeon.net>
2016-08-31 06:24:16 +0900

    * refresh 스크립트 추가

---
+++ ezerase/liveboot/scripts_host/refresh.sh
@@ -0,0 +1,4 @@
+git pull
+sudo cp ~/naraeon-ssd/ezerase/liveboot/scripts_chroot/*.sh ~/naraeon-ssd/ezerase/liveboot/chroot_init/scripts/
+sudo rm -rf ~/naraeon-ssd/ezerase/liveboot/chroot/
+sudo cp -r ~/naraeon-ssd/ezerase/liveboot/chroot_init/ ~/naraeon-ssd/ezerase/liveboot/chroot/
