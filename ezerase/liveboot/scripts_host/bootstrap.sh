4baab98f15822b663b4ddddcb20801eaf1f6fe88
Minkyu Kim <external@naraeon.net>
2016-08-30 19:37:34 +0900

    * debootstrap 앞에 root 달아줌

--- ezerase/liveboot/scripts_host/bootstrap.sh
+++ ezerase/liveboot/scripts_host/bootstrap.sh
@@ -2,6 +2,6 @@ cd ~/naraeon-ssd/ezerase/liveboot
 rm -rf chroot
 sudo apt-get install --yes debootstrap debian-archive-keyring syslinux isolinux xorriso
 sudo apt-get dist-upgrade
-debootstrap --arch=amd64 --variant=minbase stretch chroot http://ftp.kr.debian.org/debian/
+sudo debootstrap --arch=amd64 --variant=minbase stretch chroot http://ftp.kr.debian.org/debian/
 mkdir ~/naraeon-ssd/ezerase/liveboot/chroot/scripts
 sudo cp ~/naraeon-ssd/ezerase/liveboot/scripts_chroot/*.sh ~/naraeon-ssd/ezerase/liveboot/chroot/scripts/
