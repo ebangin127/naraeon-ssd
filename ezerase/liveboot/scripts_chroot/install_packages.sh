51c878992e72d35c95e858d28760998564a28b8e
Minkyu Kim <external@naraeon.net>
2016-08-30 21:52:28 +0900

    * 설치 순서 변경

--- ezerase/liveboot/scripts_chroot/install_packages.sh
+++ ezerase/liveboot/scripts_chroot/install_packages.sh
@@ -6,5 +6,5 @@ util-linux
 apt-get -t jessie-backports --no-install-recommends --yes install \
 linux-image-amd64 &&
 apt-get -t jessie --no-install-recommends --yes install \
-live-boot python3-tk xterm nano hdparm xli slim blackbox \
-xserver-xorg-core xserver-xorg xinit fonts-nanum
+live-boot xserver-xorg-core xserver-xorg xinit xterm \
+nano hdparm python3-tk xli slim blackbox fonts-nanum
