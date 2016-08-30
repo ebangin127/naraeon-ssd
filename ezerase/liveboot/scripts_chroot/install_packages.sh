7a2c9e4901f84591c987d36f05ec1321aad68663
Minkyu Kim <external@naraeon.net>
2016-08-31 06:17:59 +0900

    * live-boot jessie 백포트로 변경

--- ezerase/liveboot/scripts_chroot/install_packages.sh
+++ ezerase/liveboot/scripts_chroot/install_packages.sh
@@ -1,10 +1,10 @@
 export HOME=/root
 export LC_ALL=C
 cd /
-apt-get -t stretch --no-install-recommends --yes install \
-util-linux
-apt-get -t jessie-backports --no-install-recommends --yes install \
-linux-image-amd64 &&
-apt-get -t jessie --no-install-recommends --yes install \
-live-boot xserver-xorg-core xserver-xorg xinit xterm \
+apt-get -t jessie-backports --yes install \
+linux-image-amd64 live-boot &&
+apt-get -t jessie --yes install \
+xserver-xorg-core xserver-xorg xinit xterm \
 nano hdparm python3-tk xli slim blackbox fonts-nanum
+apt-get -t stretch --yes install \
+util-linux
