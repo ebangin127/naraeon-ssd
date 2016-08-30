467da6f9cf3a6bdb7db8663fd5adde4d1c73bcdc
Minkyu Kim <external@naraeon.net>
2016-08-31 08:23:51 +0900

    * 부트 준비 과정 담당하는 스크립트 작성

---
+++ ezerase/liveboot/scripts_chroot/release_stage0.sh
@@ -0,0 +1,3 @@
+~/naraeon-ssd/ezerase/liveboot/scripts_host/copy_settings.sh
+~/naraeon-ssd/ezerase/liveboot/scripts_host/create_grubfiles.sh
+~/naraeon-ssd/ezerase/liveboot/scripts_host/create_bootfiles.sh
