67aa7d4c48c8cc55f22eae609aca6846f58f4a56
Minkyu Kim <external@naraeon.net>
2016-08-30 20:10:23 +0900

    * release 폴더 만들기에 앞서 지우게 만듦

--- ezerase/liveboot/scripts_host/release_stage1.sh
+++ ezerase/liveboot/scripts_host/release_stage1.sh
@@ -2,7 +2,7 @@ cd ~/naraeon-ssd/ezerase/liveboot
 sudo cp -r /mnt/windows/bg.jpg chroot/root/bg.jpg
 mkdir chroot/etc/ezerase
 sudo cp -r /mnt/windows/source/*.py chroot/etc/ezerase/
-mkdir chroot_release
+sudo rm -rf chroot_release && mkdir chroot_release
 sudo cp -r chroot/* chroot_release/
 sudo mount -o bind /dev chroot_release/dev && sudo cp /etc/resolv.conf chroot_release/etc/resolv.conf
 sudo chroot chroot_release
