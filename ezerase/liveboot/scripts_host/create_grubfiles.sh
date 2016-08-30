ea39c5e2f900ef5387827bdb00b078d8fd261259
Minkyu Kim <external@naraeon.net>
2016-08-30 20:08:09 +0900

    * isolinux 폴더 없어도 생성하게 수정

--- ezerase/liveboot/scripts_host/create_grubfiles.sh
+++ ezerase/liveboot/scripts_host/create_grubfiles.sh
@@ -4,6 +4,7 @@ ntfscomp hfsplus fat ext2 normal chain boot configfile linux multiboot iso9660 g
 efi_uga loadbios fixvideo png ext2 ntfscomp loopback search minicmd cat cpuid appleldr elf usb videotest halt \
 help ls reboot echo test normal sleep memdisk tar font video_fb video gettext true video_bochs video_cirrus \
 multiboot2 acpi gfxterm_background gfxterm_menu
+mkdir -p ~/naraeon-ssd/ezerase/liveboot/image/isolinux/
 rm ~/naraeon-ssd/ezerase/liveboot/image/isolinux/efiboot.img
 mkdosfs -F12 -n "NSTERASE" -C ~/naraeon-ssd/ezerase/liveboot/image/isolinux/efiboot.img 2048
 mcopy -s -i ~/naraeon-ssd/ezerase/liveboot/image/isolinux/efiboot.img ~/naraeon-ssd/ezerase/liveboot/image/EFI ::
