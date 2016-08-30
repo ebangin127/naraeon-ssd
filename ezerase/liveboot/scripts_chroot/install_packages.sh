d51c6961c9604134aafa4cdaf7fb6ad5286a1ab6
Minkyu Kim <external@naraeon.net>
2016-08-30 20:23:16 +0900

    * /bin/init 문제 해결

--- ezerase/liveboot/scripts_chroot/install_packages.sh
+++ ezerase/liveboot/scripts_chroot/install_packages.sh
@@ -3,6 +3,6 @@ export LC_ALL=C
 cd /
 apt-get install -t sid --no-install-recommends --yes util-linux &&
 apt-get -t stretch --no-install-recommends --yes install \
-linux-image-amd64 live-boot \
+linux-image-amd64 live-boot systemd,systemd-sysv \
 xserver-xorg-core xserver-xorg xinit python3-tk \
 xterm nano hdparm xli slim blackbox fonts-nanum
