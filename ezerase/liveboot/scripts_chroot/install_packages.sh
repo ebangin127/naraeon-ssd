3d662af89abc2f29f0f7cecf3a654fcd1d47f641
Minkyu Kim <external@naraeon.net>
2016-08-30 21:04:16 +0900

    * 원래대로 (jessie) 돌림

--- ezerase/liveboot/scripts_chroot/install_packages.sh
+++ ezerase/liveboot/scripts_chroot/install_packages.sh
@@ -2,7 +2,9 @@ export HOME=/root
 export LC_ALL=C
 cd /
 apt-get install -t sid --no-install-recommends --yes \
-util-linux linux-image-amd64 live-boot systemd systemd-sysv \
+util-linux
+apt-get -t stretch --no-install-recommends --yes install
+linux-image-amd64 live-boot systemd systemd-sysv \
 xserver-xorg-core xserver-xorg xinit &&
-apt-get -t stretch --no-install-recommends --yes install \
+apt-get -t jessie --no-install-recommends --yes install \
 python3-tk xterm nano hdparm xli slim blackbox fonts-nanum
