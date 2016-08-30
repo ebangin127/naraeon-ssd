ee707cdd60c10c70a3daae1ec8a6b5086b67e51d
Minkyu Kim <external@naraeon.net>
2016-08-30 21:19:08 +0900

    * 최신 패키지 공급처 stretch로 변경

--- ezerase/liveboot/scripts_chroot/install_packages.sh
+++ ezerase/liveboot/scripts_chroot/install_packages.sh
@@ -1,9 +1,9 @@
 export HOME=/root
 export LC_ALL=C
 cd /
-apt-get install -t sid --no-install-recommends --yes \
+apt-get install -t stretch --no-install-recommends --yes \
 util-linux
-apt-get -t stretch --no-install-recommends --yes install
+apt-get -t jessie-backports --no-install-recommends --yes install
 linux-image-amd64 live-boot systemd systemd-sysv \
 xserver-xorg-core xserver-xorg xinit &&
 apt-get -t jessie --no-install-recommends --yes install \
