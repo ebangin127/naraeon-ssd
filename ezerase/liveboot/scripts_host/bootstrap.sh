d542e5bf3bd3941a2efb68df155df6f1df3a00ab
Minkyu Kim <external@naraeon.net>
2016-08-30 20:37:36 +0900

    * sid에서 모든 커널 리소스 가져오게 수정

--- ezerase/liveboot/scripts_host/bootstrap.sh
+++ ezerase/liveboot/scripts_host/bootstrap.sh
@@ -3,6 +3,6 @@ sudo rm -rf chroot
 sudo apt-get update
 sudo apt-get install --yes debootstrap debian-archive-keyring syslinux isolinux xorriso
 sudo apt-get dist-upgrade
-sudo debootstrap --arch=amd64 --variant=minbase stretch chroot http://ftp.kr.debian.org/debian/
+sudo debootstrap --arch=amd64 --variant=minbase sid chroot http://ftp.kr.debian.org/debian/
 sudo mkdir ~/naraeon-ssd/ezerase/liveboot/chroot/scripts
 sudo cp ~/naraeon-ssd/ezerase/liveboot/scripts_chroot/*.sh ~/naraeon-ssd/ezerase/liveboot/chroot/scripts/
