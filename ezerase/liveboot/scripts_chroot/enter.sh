168401d3a2e7f0226c599d58e31eed9f7764c90f
Minkyu Kim <external@naraeon.net>
2016-08-30 21:13:04 +0900

    * backport 에러 해결

--- ezerase/liveboot/scripts_chroot/enter.sh
+++ ezerase/liveboot/scripts_chroot/enter.sh
@@ -5,8 +5,8 @@ mount none -t devpts /dev/pts
 export HOME=/root 
 export LC_ALL=C 
 echo "deb http://ftp.kr.debian.org/debian/ sid main" >> /etc/apt/sources.list
-echo "deb http://ftp.kr.debian.org/debian/ jessie-backports main" >> /etc/apt/sources.list
 echo "deb http://ftp.kr.debian.org/debian/ jessie main" >> /etc/apt/sources.list
+echo "deb http://ftp.kr.debian.org/debian/ jessie-backports main" >> /etc/apt/sources.list
 apt-get update && 
 apt-get install dialog dbus -t jessie --yes && 
 dbus-uuidgen > /var/lib/dbus/machine-id && 
