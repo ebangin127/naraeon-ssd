b5ca79690e45856fd67f2694fd8f2a98904ca83d
Minkyu Kim <external@naraeon.net>
2016-08-31 08:59:49 +0900

    * 완전 부트스트랩 버그 수정

--- ezerase/liveboot/scripts_host/complete_bootstrap.sh
+++ ezerase/liveboot/scripts_host/complete_bootstrap.sh
@@ -7,4 +7,4 @@ git clone https://github.com/linux-nvme/nvme-cli.git
 wget https://github.com/Drive-Trust-Alliance/exec/blob/master/sedutil_LINUX.tgz?raw=true
 mv sedutil_LINUX.tgz?raw=true sedutil.tgz
 tar zxvf sedutil.tgz
-./naraeon-ssd/ezerase/liveboot/scripts_host/bootstrap.sh
+~/naraeon-ssd/ezerase/liveboot/scripts_host/bootstrap.sh
