8c3a8ba8580b14b3b230cdd00c9765d6fe3ab74e
Minkyu Kim <external@naraeon.net>
2016-08-30 21:37:25 +0900

    * 설치 가능한 부분까지 완료

--- ezerase/liveboot/scripts_chroot/install_packages.sh
+++ ezerase/liveboot/scripts_chroot/install_packages.sh
@@ -4,6 +4,7 @@ cd /
 apt-get -t stretch --no-install-recommends --yes install \
 util-linux
 apt-get -t jessie-backports --no-install-recommends --yes install \
-linux-image-amd64 xserver-xorg-core xserver-xorg xinit &&
+linux-image-amd64 &&
 apt-get -t jessie --no-install-recommends --yes install \
-live-boot python3-tk xterm nano hdparm xli slim blackbox fonts-nanum
+live-boot python3-tk xterm nano hdparm xli slim blackbox \
+xserver-xorg-core xserver-xorg xinit fonts-nanum
