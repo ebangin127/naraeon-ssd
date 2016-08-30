643176833eb13532437624dbfc30fb396502ccd2
Minkyu Kim <external@naraeon.net>
2016-08-30 21:26:28 +0900

    * 패키지 옵션 변경

--- ezerase/liveboot/scripts_chroot/install_packages.sh
+++ ezerase/liveboot/scripts_chroot/install_packages.sh
@@ -3,8 +3,8 @@ export LC_ALL=C
 cd /
 apt-get install -t stretch --no-install-recommends --yes \
 util-linux
-apt-get -t jessie-backports --no-install-recommends --yes install
+apt-get install -t jessie-backports --no-install-recommends --yes 
 linux-image-amd64 live-boot systemd systemd-sysv \
 xserver-xorg-core xserver-xorg xinit &&
-apt-get -t jessie --no-install-recommends --yes install \
+apt-get install -t jessie --no-install-recommends --yes \
 python3-tk xterm nano hdparm xli slim blackbox fonts-nanum
