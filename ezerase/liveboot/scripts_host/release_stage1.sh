d6223e188d676e79ab89bfb5148b343d411b8bc3
Minkyu Kim <external@naraeon.net>
2016-08-30 20:12:57 +0900

    * mkdir sudo 권한으로 수행하게 조정

--- ezerase/liveboot/scripts_host/release_stage1.sh
+++ ezerase/liveboot/scripts_host/release_stage1.sh
@@ -1,6 +1,6 @@
 cd ~/naraeon-ssd/ezerase/liveboot
 sudo cp -r /mnt/windows/bg.jpg chroot/root/bg.jpg
-mkdir chroot/etc/ezerase
+sudo mkdir chroot/etc/ezerase
 sudo cp -r /mnt/windows/source/*.py chroot/etc/ezerase/
 sudo rm -rf chroot_release && mkdir chroot_release
 sudo cp -r chroot/* chroot_release/
