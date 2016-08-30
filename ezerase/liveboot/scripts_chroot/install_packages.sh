92aa22d1e574b2d193319e86e8c7a9342b055753
Minkyu Kim <external@naraeon.net>
2016-08-30 21:32:52 +0900

    * 패키지 태그 변경

--- ezerase/liveboot/scripts_chroot/install_packages.sh
+++ ezerase/liveboot/scripts_chroot/install_packages.sh
@@ -4,7 +4,6 @@ cd /
 apt-get -t stretch --no-install-recommends --yes install \
 util-linux
 apt-get -t jessie-backports --no-install-recommends --yes install \
-linux-image-amd64 live-boot systemd systemd-sysv \
-xserver-xorg-core xserver-xorg xinit &&
+linux-image-amd64 xserver-xorg-core xserver-xorg xinit &&
 apt-get -t jessie --no-install-recommends --yes install \
-python3-tk xterm nano hdparm xli slim blackbox fonts-nanum
+live-boot python3-tk xterm nano hdparm xli slim blackbox fonts-nanum
