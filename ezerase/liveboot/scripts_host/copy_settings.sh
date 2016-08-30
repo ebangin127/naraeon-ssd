461e6288f1de29b5c6f53005bf21366553111e83
Minkyu Kim <external@naraeon.net>
2016-08-30 20:04:32 +0900

    * cp시 sudo 하게 수정

--- ezerase/liveboot/scripts_host/copy_settings.sh
+++ ezerase/liveboot/scripts_host/copy_settings.sh
@@ -5,11 +5,10 @@ sudo cp /mnt/windows/dependencies/.blackboxrc ~/naraeon-ssd/ezerase/liveboot/con
 sudo cp /mnt/windows/dependencies/.bsetbgrc ~/naraeon-ssd/ezerase/liveboot/config/
 sudo chown ebangin127 ~/naraeon-ssd/ezerase/liveboot/config/*
 cd ~/naraeon-ssd/ezerase/liveboot/config
-cp ./slim.conf ../chroot/etc/
-cp ./blackbox-menu ../chroot/etc/X11/blackbox/blackbox-menu
-cp ./Gray ../chroot/usr/share/blackbox/styles/Gray
-cp ./.bashrc ../chroot/root/
-cp ./.blackboxrc ../chroot/root/
-cp ./.bsetbgrc ../chroot/root/
+sudo cp ./slim.conf ../chroot/etc/
+sudo cp ./blackbox-menu ../chroot/etc/X11/blackbox/blackbox-menu
+sudo cp ./Gray ../chroot/usr/share/blackbox/styles/Gray
+sudo cp ./.bashrc ../chroot/root/
+sudo cp ./.blackboxrc ../chroot/root/
+sudo cp ./.bsetbgrc ../chroot/root/
 cd ..
-
