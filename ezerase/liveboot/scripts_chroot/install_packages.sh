e1be91ff610472d5dae78ff002c895e257744b43
Minkyu Kim <external@naraeon.net>
2016-08-31 09:52:56 +0900

    * video-all 미리 깔아놓고 시작하게 변경

--- ezerase/liveboot/scripts_chroot/install_packages.sh
+++ ezerase/liveboot/scripts_chroot/install_packages.sh
@@ -2,11 +2,9 @@ export HOME=/root
 export LC_ALL=C
 cd /
 apt-get -t jessie-backports --no-install-recommends --yes install \
-linux-image-amd64 live-boot
+linux-image-amd64 live-boot xserver-xorg-video-all
 apt-get -t jessie --no-install-recommends --yes install \
 xserver-xorg-core xserver-xorg xinit xterm \
 nano hdparm python3-tk xli slim blackbox fonts-nanum
-apt-get -t jessie-backports --no-install-recommends --yes install \
-xserver-xorg-video-nouveau
 apt-get -t stretch --no-install-recommends --yes install \
 util-linux
