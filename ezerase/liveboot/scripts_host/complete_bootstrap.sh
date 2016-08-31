bf7cec24054775ce5af527ef1fcfcec79e204582
Minkyu Kim <external@naraeon.net>
2016-08-31 09:00:50 +0900

    * 갱신까지 되게 수정

--- ezerase/liveboot/scripts_host/complete_bootstrap.sh
+++ ezerase/liveboot/scripts_host/complete_bootstrap.sh
@@ -2,8 +2,14 @@ cd ~
 sudo apt-get update
 sudo apt-get --yes git
 git clone git@github.com:ebangin127/naraeon-ssd.git
+cd naraeon-ssd
+git pull
+cd ..
 cd ./naraeon-ssd/ezerase/liveboot/
 git clone https://github.com/linux-nvme/nvme-cli.git
+cd nvme-cli
+git pull
+cd ..
 wget https://github.com/Drive-Trust-Alliance/exec/blob/master/sedutil_LINUX.tgz?raw=true
 mv sedutil_LINUX.tgz?raw=true sedutil.tgz
 tar zxvf sedutil.tgz
