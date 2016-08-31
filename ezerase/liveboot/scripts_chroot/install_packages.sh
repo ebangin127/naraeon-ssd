24642ba4450a37ef958c7ce535696c056f46ec50
Minkyu Kim <external@naraeon.net>
2016-08-31 10:12:49 +0900

    * 원래대로 돌림

--- ezerase/liveboot/scripts_chroot/install_packages.sh
+++ ezerase/liveboot/scripts_chroot/install_packages.sh
@@ -2,7 +2,7 @@ export HOME=/root
 export LC_ALL=C
 cd /
 apt-get -t jessie-backports --no-install-recommends --yes install \
-linux-image-amd64 live-boot xserver-xorg-video-all xserver-xorg-core xserver-xorg blackbox xinit
+linux-image-amd64 live-boot xserver-xorg-core xserver-xorg blackbox xinit
 apt-get -t jessie --no-install-recommends --yes install \
 xterm nano hdparm python3-tk xli slim fonts-nanum
 apt-get -t stretch --no-install-recommends --yes install \
