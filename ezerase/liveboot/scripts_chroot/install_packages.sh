8a46802f1ec034b276a02f0a75be9d7bd6dadf84
Minkyu Kim <external@naraeon.net>
2016-08-31 10:21:18 +0900

    * xorg 다시 jessie로 옮김

--- ezerase/liveboot/scripts_chroot/install_packages.sh
+++ ezerase/liveboot/scripts_chroot/install_packages.sh
@@ -2,8 +2,9 @@ export HOME=/root
 export LC_ALL=C
 cd /
 apt-get -t jessie-backports --no-install-recommends --yes install \
-linux-image-amd64 live-boot xserver-xorg-core xserver-xorg blackbox xinit
+linux-image-amd64 live-boot
 apt-get -t jessie --no-install-recommends --yes install \
-xterm nano hdparm python3-tk xli slim fonts-nanum
+xserver-xorg-core xserver-xorg blackbox xinit xterm \
+nano hdparm python3-tk xli slim fonts-nanum
 apt-get -t stretch --no-install-recommends --yes install \
 util-linux
