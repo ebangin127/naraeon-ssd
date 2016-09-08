4b2d2361978d7bb7010e329bcbd6b86fd52980a0
Minkyu Kim <external@naraeon.net>
2016-09-08 23:15:55 +0900

    * xrandr 테스트중

--- ezerase/liveboot/scripts_chroot/install_packages.sh
+++ ezerase/liveboot/scripts_chroot/install_packages.sh
@@ -2,9 +2,9 @@ export HOME=/root
 export LC_ALL=C
 cd /
 apt-get -t jessie-backports --no-install-recommends --yes install \
-linux-image-amd64 live-boot
+linux-image-amd64 live-boot xserver-xorg-core xserver-xorg xterm \
+x11-xserver-utils
 apt-get -t jessie --no-install-recommends --yes install \
-xserver-xorg-core xserver-xorg blackbox xinit xterm \
-nano hdparm python3-tk xli slim fonts-nanum
+blackbox xinit nano hdparm python3-tk xli slim fonts-nanum
 apt-get -t stretch --no-install-recommends --yes install \
 util-linux
