23a15fba4af5366d5dc31340eb46a5f0b0a5634c
Minkyu Kim <external@naraeon.net>
2016-09-09 09:19:34 +0900

    * strectch로 롤백

--- ezerase/liveboot/scripts_chroot/install_packages.sh
+++ ezerase/liveboot/scripts_chroot/install_packages.sh
@@ -1,7 +1,7 @@
 export HOME=/root
 export LC_ALL=C
 cd /
-apt-get -t sid --no-install-recommends --yes install \
+apt-get -t stretch --no-install-recommends --yes install \
 linux-image-amd64 xserver-xorg-core xserver-xorg xterm \
-x11-xserver-utils xinit util-linux live-boot \
+x11-xserver-utils xinit util-linux live-boot systemd systemd-sysv \
 blackbox nano hdparm python3-tk xli slim fonts-nanum
