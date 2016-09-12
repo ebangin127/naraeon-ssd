c14f5113ed6118cc89fa0fa3c5a200047597f27d
Minkyu Kim <external@naraeon.net>
2016-09-12 19:57:48 +0900

    * Changed tabs to spaces

--- ezerase/liveboot/config/99_resume.sh
+++ ezerase/liveboot/config/99_resume.sh
@@ -2,7 +2,7 @@
 
 case "$1" in
     resume)
-		EXTERNAL=$(xrandr | grep -E " connected" | sed -e "s/\([A-Z0-9]\+\") connected.*/\1/")
+        EXTERNAL=$(xrandr | grep -E " connected" | sed -e "s/\([A-Z0-9]\+\") connected.*/\1/")
         xrandr --output $EXTERNAL --off
         xrandr --output $EXTERNAL --auto
 esac
