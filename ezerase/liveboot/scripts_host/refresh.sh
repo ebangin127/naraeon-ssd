9e1b4d24d23e5bdd58dd91806c56cdda6b5e3565
Minkyu Kim <external@naraeon.net>
2017-01-03 10:17:52 +0900

    * Bootstrap 등에 nvidia 반영

--- ezerase/liveboot/scripts_host/refresh.sh
+++ ezerase/liveboot/scripts_host/refresh.sh
@@ -3,3 +3,6 @@ git pull
 sudo cp ~/naraeon-ssd/ezerase/liveboot/scripts_chroot/*.sh ~/naraeon-ssd/ezerase/liveboot/chroot_init/scripts/
 sudo rm -rf ~/naraeon-ssd/ezerase/liveboot/chroot/
 sudo cp -r ~/naraeon-ssd/ezerase/liveboot/chroot_init/ ~/naraeon-ssd/ezerase/liveboot/chroot/
+sudo cp ~/naraeon-ssd/ezerase/liveboot/scripts_chroot/*.sh ~/naraeon-ssd/ezerase/liveboot/chroot_nvidia_init/scripts/
+sudo rm -rf ~/naraeon-ssd/ezerase/liveboot/chroot_nvidia/
+sudo cp -r ~/naraeon-ssd/ezerase/liveboot/chroot_nvidia_init/ ~/naraeon-ssd/ezerase/liveboot/chroot_nvidia/
