259bb2c7c5a2a663e7bea4433b5bcbda645943aa
Minkyu Kim <external@naraeon.net>
2017-01-03 11:07:01 +0900

    * xfce4-screenshooter 추가

--- ezerase/liveboot/scripts_chroot/install_packages.sh
+++ ezerase/liveboot/scripts_chroot/install_packages.sh
@@ -4,4 +4,4 @@ cd /
 apt-get -t stretch --no-install-recommends --yes install \
 linux-image-amd64 xserver-xorg-core xserver-xorg xterm \
 x11-xserver-utils xinit util-linux live-boot systemd systemd-sysv \
-blackbox nano hdparm python3-tk xli slim fonts-nanum pm-utils
+blackbox nano hdparm python3-tk xli slim fonts-nanum pm-utils xfce4-screenshooter
