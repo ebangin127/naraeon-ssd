a2a7394c3839ab214a2958e2a0679455fa8aa7a8
Minkyu Kim <external@naraeon.net>
2017-01-03 12:02:33 +0900

    * tmp 권한 수정

--- ezerase/liveboot/scripts_chroot/enter.sh
+++ ezerase/liveboot/scripts_chroot/enter.sh
@@ -10,3 +10,4 @@ apt-get install dialog dbus -t stretch --yes &&
 dbus-uuidgen > /var/lib/dbus/machine-id && 
 echo "naraeon-live" > /etc/hostname
 chmod 755 /usr/lib/pm-utils/sleep.d/*.sh
+chmod 777 /tmp
